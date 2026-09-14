780b70c5b45532f06adac57595c10322e3b2ea14
RangS-1 <rangga19sj@gmail.com>
2026-09-14 19:08:33 +0700

    Scripts: rrc-cli.py for install, show templates list

---
+++ scripts/rrc-cli.py
@@ -0,0 +1,57 @@
+import os
+from colorama import Fore
+from pathlib import Path as pt
+
+def install():
+    home_dir = pt.home() / ".rrc"
+    if home_dir.exists():
+        success()
+        print(f".rrc directory on {home_dir} exists.")
+    else:
+        check()
+        print(f".rrc directory on {home_dir} does not exist!")
+        check()
+        update = input("Would you like to install it? (y/n): ")
+        if update.lower() == 'y':
+            home_dir.mkdir(parents=True, exist_ok=True)
+            print(f".rrc directory created at {home_dir}.")
+            os.system("git clone https://github.com/RangS-1/rrc.git ~/.rrc")
+        else:
+            failed()
+            print("Installation Skipped...")
+
+def list():
+    home_dir = pt.home() / ".rrc"
+    exclude_folder = '.git'
+
+    for dirpath, dirnames, filenames in os.walk(home_dir):
+        dirnames[:] = [d for d in dirnames if d != exclude_folder]
+
+        for dirname in dirnames:
+            print(os.path.join(dirpath, dirname))
+
+
+def check():
+    print(Fore.YELLOW, "[!]", Fore.BLUE, end="")
+
+def success():
+    print(Fore.GREEN, "[✓]", Fore.BLUE, end="")
+
+def failed():
+    print(Fore.RED, "[X]", Fore.BLUE, end="")
+
+def main():
+    while True:
+        check()
+        choose_templates = input(f"rrc_cli> {Fore.WHITE}")
+        if choose_templates == "list":
+            list()
+        elif choose_templates == "install":
+            install()
+        elif choose_templates == "exit":
+            success()
+            print("See You Soon!")
+            break
+
+if __name__ == "__main__":
+    main()
