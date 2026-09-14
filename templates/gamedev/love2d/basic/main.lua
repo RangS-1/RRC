fc327781d78376c54e9553bd1f1bb2713d233860
RangS-1 <rangga19sj@gmail.com>
2026-09-14 14:45:27 +0700

    Gamedev: love2d basic. Add player movement

--- templates/gamedev/love2d/basic/main.lua
+++ templates/gamedev/love2d/basic/main.lua
@@ -1,13 +1,30 @@
 _G.love = require("love")
 
 function love.load()
-
+    player = {}
+    player.x = 100
+    player.y = 100
 end
 
 function love.update(dt)
+    speed = 100
+    if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
+        player.x = player.x + speed
+    end
+    
+    if love.keyboard.isDown("left") or love.keyboard.isDown("a") then
+        player.x = player.x - speed
+    end
+    
+    if love.keyboard.isDown("up") or love.keyboard.isDown("w") then
+        player.y = player.y - speed
+    end
 
+    if love.keyboard.isDown("down") or love.keyboard.isDown("s") then
+        player.y = player.y + speed
+    end
 end
 
 function love.draw()
-    love.graphics.print("Hi Dad")
+    player_sprite = love.graphics.circle("fill", player.x, player.y, 25)
 end
