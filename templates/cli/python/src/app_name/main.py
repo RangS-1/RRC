bc35f06c90b662ad11ac0344317eea61e95b854c
RangS-1 <rangga19sj@gmail.com>
2026-09-17 17:39:49 +0700

    Feat: loop with if statements!

--- templates/cli/python/src/app_name/main.py
+++ templates/cli/python/src/app_name/main.py
@@ -1,8 +1,12 @@
 import os
 
 def main():
-    print("AppName")
-
+    while True:
+        choose_templates = input(f"cli>")
+        if choose_templates == "list":
+            print("Hello")
+        else:
+            pass
 
 if __name__ == '__main__':
     main()
